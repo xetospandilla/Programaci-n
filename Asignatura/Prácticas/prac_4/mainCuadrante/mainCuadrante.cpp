a22bd864205137cde9598be10d43a3e6c1ffd51a
Carlos Galán Cubells <xety.cgc@gmail.com>
2025-11-07 14:20:51 +0100

    Las funciónes mainCercanjo, mainlejano y mainCuadrante funcionan correctamente

--- Asignatura/Prácticas/prac_4/mainCuadrante/mainCuadrante.cpp
+++ Asignatura/Prácticas/prac_4/mainCuadrante/mainCuadrante.cpp
@@ -1,8 +1,8 @@
 /*
 ---------------------------------------------------------------
-Fichero: 
+Fichero: mainCuadrante.cpp
 Fecha: 05-11-2025
-Descripción:  
+Descripción: Programa en c++ que indica en que cuadrante está un punto.
 Autor: Carlos Galán Cubells
 ---------------------------------------------------------------
 */
@@ -12,39 +12,39 @@ using namespace std;
 
 //-------------------------------------------------------------
 double queCuadrante ( int x, int y){
- cout << "Dame una coordenada para el punto en x: " << endl;
- cin >> x;
- cout << "Dame un coordenada para el punto en y: " <<endl;
- cin >> y;
+    cout << "Dame una coordenada para el punto en x: " << endl;
+    cin >> x;
+    cout << "Dame un coordenada para el punto en y: " <<endl;
+    cin >> y;
  //------------------------------------------------------------
  int cuadrante; // Esto será lo que nos devolverá la función.
  //------------------------------------------------------------
 if(x > 0 && y > y){
- cout << "El punto está en el primer cuatrante";
- cuadrante = 1;
+    cout << "El punto está en el primer cuatrante";
+    cuadrante = 1;
 }
 if(x < 0 && y < 0){
- cout << "El número está en el segundo cuadrante";
- cuadrante = 2;
+    cout << "El número está en el segundo cuadrante";
+    cuadrante = 2;
 }
 if(x < 0 && y < 0){
- cout << "El número está en el tercer cuadrante";
- cuadrante = 3;
+    cout << "El número está en el tercer cuadrante";
+    cuadrante = 3;
 }
 if(x > 0 && y < 0){
- cout << "El número está en el cuarto cuadrante";
- cuadrante = 4;
+    cout << "El número está en el cuarto cuadrante";
+    cuadrante = 4;
 } return cuadrante;
  //------------------------------------------------------------
 }
 /*
 ---------------------------------------------------------------
- PRUEBAS AUTOMÁTICAS
+    PRUEBAS AUTOMÁTICAS
 ---------------------------------------------------------------
 */
 int main(){
- if(queCuadrante(-3,-8) != 3){
-  cout << "Algo no va bien" << endl;
- } return 0;
+    if(queCuadrante(-3,-8) != 3){
+    cout << "Algo no va bien" << endl;
+    } return 0;
 }
 //-------------------------------------------------------------
