408a684e819b6cac8838fd7b862e7d2c5487b5af
Carlos Galán Cubells <xety.cgc@gmail.com>
2025-11-09 23:04:00 +0100

    Todo ordenado dentro del curso de C++ y el ejercicio GestorNotas.cpp por comenzar.

---
+++ Cursos/C++/Prácticas/2-GestorNotas.cpp
@@ -0,0 +1,25 @@
+/*
+-------------------------------------------------------------------------------------------------------
+Objetivo:
+
+Crea un programa en C++ que gestione las notas de varios alumnos.
+Cada alumno puede tener varias asignaturas, y de cada asignatura se guardarán varias notas.
+
+Tendrás que permitir al usuario:
+
+1. Añadir un alumno nuevo.
+2. Añadir una asignatura a un alumno.
+3. Introducir notas en una asignatura.
+4. Mostrar todas las notas de un alumno.
+5. Mostrar la media general del alumno (media de medias de sus asignaturas).
+6. Mostrar la mejor nota del alumno y la peor.
+7. Salir.
+-------------------------------------------------------------------------------------------------------
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class gestor{
+
+};
