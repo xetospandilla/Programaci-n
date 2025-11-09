0ad9ff09aa20ae8856877770bde1dfc00dc7688e
Carlos Galán Cubells <xety.cgc@gmail.com>
2025-11-09 03:52:21 +0100

    Hice apuntes de vetores, siwtch-case y arrays.

---
+++ Cursos/C++/Switch-Case/switch-case.cpp
@@ -0,0 +1,27 @@
+/*
+-------------------------------------------------------------------------------------------------------
+Siwtch-Case
+-------------------------------------------------------------------------------------------------------
+- la instrucción switch-case es una instrucción condicional especialmente útil cuando se necesita
+realizar acciones específicas según el valor de una variable, sin la necesidad de encadenar múltiples
+sentencias if-else.
+
+- La sintaxis báisca de la instrución sería la siguiente:
+*/ 
+#include <iostream>
+using namespace std;
+
+switch (variable) {
+    case valor1:
+    //Bloque de código para el valor1.
+    case valor2:
+    //Bloque de código para el valor2.
+    break;
+//Más casos.
+default:
+    //Bloque de código para el caso cuando no se ningún caso anterior.
+}
+/*
+-------------------------------------------------------------------------------------------------------
+*/
+
