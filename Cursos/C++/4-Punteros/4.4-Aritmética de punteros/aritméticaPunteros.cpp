cb37cfd5a58dedf3460387102491b2d7f2facf5a
xety <xety.cgc@gmail.com>
2025-11-13 12:22:29 +0000

    Comenzando el video 45, pendiente de terminar está noche

---
+++ Cursos/C++/4-Punteros/4.4-Aritmética de punteros/aritméticaPunteros.cpp
@@ -0,0 +1,16 @@
+/*
+-------------------------------------------------------------------------------------------------------------------------------
+Aritmética de punteros
+-------------------------------------------------------------------------------------------------------------------------------
+- En C++ un array es básicamente un puntero al primer elemento del array.
+
+ - El valor del nombre del array es la dirección en memoría del primer elemento del arrya.
+
+ - CUando se usa el nombre del array sin ningún índice se obtiene la dirección en memoría del primer elemento del array.
+
+ - Cuando un puntero apunta al mismo tipo de dato que los elementos de un array, es casi en todos los escenarios,
+ puedes usar el nombre del puntero y el nombre del array intercambiable para realizar operaciones similares,
+ especialmente en lo que respecta a la indexación y aritmética de punteros. Sin embargo, el "casi" indica
+ que hay excepciones a esta regla.
+-------------------------------------------------------------------------------------------------------------------------------
+*/
---
+++ Cursos/C++/Prácticas/4-punteros_basico.cpp
@@ -0,0 +1,56 @@
+/*
+-----------------------------------------------------------------------------------------------------
+punteros_basico.cpp
+-----------------------------------------------------------------------------------------------------
+📝 Ejercicio: Gestión de enteros con punteros
+
+ - Enunciado:
+
+  - Crea un programa en C++ que haga lo siguiente:
+
+  - Pida al usuario cuatro números enteros y los almacene en variables normales (no en un vector).
+
+  - Cree punteros que apunten a cada uno de estos números.
+
+ - Muestre:
+
+  - El valor de cada número usando los punteros.
+
+  - La dirección de memoria de cada número.
+
+  - Cambie el valor de un número a través de su puntero y muestre de nuevo todos los valores para verificar que cambió correctamente.
+
+  - (Opcional) Ordene los números de menor a mayor usando solo punteros para acceder a los valores.
+
+  - Objetivos que cumples con este ejercicio:
+
+  - Declarar punteros y asociarlos a variables.
+
+  - Acceder a valores usando * (desreferenciación).
+
+  - Acceder a direcciones de memoria con &.
+
+  - Modificar valores desde un puntero.
+
+  - Introducción a manipulación indirecta de variables.
+-----------------------------------------------------------------------------------------------------
+*/
+
+#include <iostream>
+using namespace std;
+
+//-----------------------------------------------------------------------------------------------
+//Variables:
+int var1;
+int var2;
+int var3;
+int var4;
+//Punteros:
+int *v1 = &var1;
+int *v2 = &var2;
+int *v3 = &var3;
+int *v4 = &var4;
+//Funcion para almacenar los valores:
+void introducirValores(){
+ if(
+}
